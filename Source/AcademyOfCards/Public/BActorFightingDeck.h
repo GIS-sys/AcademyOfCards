6534db80b0a16e528189463a4d728395e2c79595
GIS-sys <67117384+GIS-sys@users.noreply.github.com>
2025-06-04 08:32:00 +0300

    almost done autofitting text

--- Source/AcademyOfCards/Public/BActorFightingDeck.h
+++ Source/AcademyOfCards/Public/BActorFightingDeck.h
@@ -23,7 +23,6 @@ public:
 	UUMyGameInstance* MyGameInstance;
 	TArray<FString> CardIDs;
 	TArray<FString> DebugCardIDs = {
-		//"c005",
 		/*"c001", "c002", "c003", "c004", "c005", "c006", "c007", "c008", "c009", "c010",
 		"c011", "c012", "c013", "c014", "c015", "c016", "c017", "c018", "c019", "c020",
 		"c021", "c022", "c023", "c024", "c025", "c026", "c027", "c028", "c029", "c030",
